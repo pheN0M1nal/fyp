378735476389131d1ccbd98b5cd8802e5e3e2491
Talha-Mazhar <ahmadnutt9111@gmail.com>
2022-10-29 23:55:19 +0500

    SignIn/SignUp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
 function App() {
-	return <div></div>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
-
 export default App
---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+function SignIn() {
+  return <div>SignIn</div>
+}
+
+export default SignIn
