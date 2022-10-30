12b36782d97baa46020edfb77201498c6dadd3a9
Talha-Mazhar <ahmadnutt9111@gmail.com>
2022-10-30 17:39:06 +0500

    TailwindConfigFix

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -3,7 +3,13 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 function SignIn() {
-  return <div>SignIn</div>
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center text-3xl font-bold underline font-sans text-blue-500'>
+        Welcome to our E-commerce Site Create Account!
+      </div>
+    </>
+  )
 }
 
 export default SignIn
