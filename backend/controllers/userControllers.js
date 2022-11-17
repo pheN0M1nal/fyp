a52b589a9e1ef73df6c716ce6d7d8f834710abc7
ShaheerAziz <h.m.shaheeraziz@gmail.com>
2022-11-17 17:23:59 +0500

    Hashing implemented in Login/SignUp

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,31 +1,37 @@
 const User = require('../models/users')
 const asyncHandler = require('express-async-handler') 
+const bcrypt = require('bcrypt')
 
 const registerUser = asyncHandler(async (req, res) => {
 
     const {myName, email, password}  = req.body
     const userExists = await User.findOne({ email })
+    const saltRounds = 10;
     if (userExists) {
 		res.status(400)
 		throw new Error('User already exist.')
 	}
     else{
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.hash(password, salt, function(err, password) {
+                // Store hash in your password DB.
+                const user = new User(
+                    {
+                        myName,
+                        email,
+                        password
+                    }
+                )
 
-        const user = new User(
-            {
-                myName,
-                email,
-                password
-            }
-        )
-        
-        user.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+                user.save()
+                .then((result) => {
+                    res.send(result)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+            });
+        });
         
     }
 })
@@ -35,16 +41,27 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const { email, password } = req.body
 	const user = await User.findOne({ email })
-    if (user && user.password == password){
-		res.json({
-			myNname: user.myName,
-			email: user.email,
-			password: user.password
-        })
+    flag = false;
+    if (user){
+        const flag = await bcrypt.compare(password, user.password);
+
+        if(flag){
+            res.json({
+                myNname: user.myName,
+                email: user.email,
+                password: password
+            })
+        }
+        else {
+            res.status(401)
+            throw new Error('Invalid Password')
+            
+        }
+
     }
 	else {
 		res.status(401)
-		throw new Error('Invalid email or password')
+		throw new Error('Invalid Email')
 	}
 })
     
