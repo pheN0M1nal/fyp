fbf9aa7bd62ebb47cea032b64c3e96bfe9fd69ac
noman <noman@nomans-MacBook-Pro.local>
2022-11-25 00:55:25 +0500

    latest chnages

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const dbURL =
+  "mongodb+srv://Shaheer:abcd1234@test.kdydufa.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(5000))
+  .catch((err) => console.log(err));
+
+app.use("/api/users", require("./routes/usersRoutes"));
