bc128c440738520d2fadd28002b32044d535265b
Talha-Mazhar <ahmadnutt9111@gmail.com>
2022-12-29 07:30:35 +0500

    only features left

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
       </Router>
     </>
   )
