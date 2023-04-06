5d7d66ff6cae748c165be7c19aafbd02ac0f1cc7
Talha Mazhar <ahmadnutt9111@gmail.com>
2023-04-06 17:07:13 +0500

    Reviews Integerated

---
+++ frontend/src/components/product/ReviewItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ReviewItem = () => {
+  return (
+    <>
+      <div>
+        {/* User name */}
+        <span>Talha</span>
+        {/* Comment */}
+        <span>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
+          cupiditate eligendi labore explicabo beatae, ea placeat cum. Eligendi
+          suscipit, unde harum aut, dignissimos excepturi dolorem incidunt,
+          molestiae aspernatur modi eum.
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default ReviewItem;
