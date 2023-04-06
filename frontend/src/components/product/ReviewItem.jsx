b6e4010664b81a75b64f988bfb5ee98dc22a4d2b
Talha Mazhar <ahmadnutt9111@gmail.com>
2023-04-06 17:11:06 +0500

    Reviews Integerated styling

--- frontend/src/components/product/ReviewItem.jsx
+++ frontend/src/components/product/ReviewItem.jsx
@@ -3,11 +3,12 @@ import React from "react";
 const ReviewItem = () => {
   return (
     <>
-      <div>
+      <div className="flex flex-col space-y-4">
         {/* User name */}
-        <span>Talha</span>
+
+        <span className="text-base font-serif text-zinc-500">Talha</span>
         {/* Comment */}
-        <span>
+        <span className="text-sm text-zinc-400">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
           cupiditate eligendi labore explicabo beatae, ea placeat cum. Eligendi
           suscipit, unde harum aut, dignissimos excepturi dolorem incidunt,
