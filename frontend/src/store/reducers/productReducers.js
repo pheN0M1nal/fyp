f3c95b7f52cfe4cb959eb7df5a8fe47c67942791
phen0m1nal <nmnashrf@gmail.com>
2023-04-14 01:28:32 +0500

    merged changes 1.1

--- frontend/src/store/reducers/productReducers.js
+++ frontend/src/store/reducers/productReducers.js
@@ -1,19 +1,20 @@
 import {
-  ALL_PRODUCT_REQUEST,
-  ALL_PRODUCT_SUCCESS,
-  ALL_PRODUCT_FAIL,
-} from "../constants/productConstants";
+    ALL_PRODUCT_REQUEST,
+    ALL_PRODUCT_SUCCESS,
+    ALL_PRODUCT_FAIL,
+} from "../constants/productConstants"
 
 //Get All Products
 export const productDetailReducer = (state = {}, action) => {
-  switch (action.type) {
-    case ALL_PRODUCT_REQUEST:
-      return { loading: true, products: action.payload };
-    case ALL_PRODUCT_SUCCESS:
-      return { loading: false, products: action.payload };
-    case ALL_PRODUCT_FAIL:
-      return { loading: false, productsInfo: {} };
-    default:
-      return state;
-  }
-};
+    switch (action.type) {
+        case ALL_PRODUCT_REQUEST:
+            return { loading: true, products: action.payload }
+        case ALL_PRODUCT_SUCCESS:
+            return { loading: false, products: action.payload }
+        case ALL_PRODUCT_FAIL:
+            return { loading: false, productsInfo: {} }
+        default:
+            return state
+    }
+}
+//
